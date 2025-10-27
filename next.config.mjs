f87a3f4e2408cbf04194b59fbd7544c59c7d4dc0
Abhishek9678 <abhishek@houseofevolve.in>
2025-10-27 15:31:43 +0530

    build error

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['lightningcss'],
+  },
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `lightningcss` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 }
 
 export default nextConfig
