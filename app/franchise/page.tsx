65e9e5b0233efcb826397313af4f3b9d422c0357
Abhishek9678 <abhishek@houseofevolve.in>
2025-10-28 14:24:39 +0530

    number

--- app/franchise/page.tsx
+++ app/franchise/page.tsx
@@ -172,7 +172,7 @@ export default function FranchisePage() {
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
               <a 
-                href="tel:+919876543210" 
+                href="tel:+919211780652" 
                 className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg hover:bg-blue-700 transition-colors"
               >
                 Call Us Now
